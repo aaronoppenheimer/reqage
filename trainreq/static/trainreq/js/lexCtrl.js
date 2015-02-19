2152b59f73e3a815a68f7445f07b1a1a3de83c3f
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2015-02-19 13:16:40 -0500

    still working

--- trainreq/static/trainreq/js/lexCtrl.js
+++ trainreq/static/trainreq/js/lexCtrl.js
@@ -2,52 +2,64 @@
 //
 // ao 2015-02-15
 
-app.controller("lexCtrl", function($scope,$http,appLexId) {
+app.controller("lexCtrl", function($scope,$http) {
 
     var that=this;
     
-    myLexId = null;
-
+//     $scope.lexId = null;
     $scope.myLex = null; // my current lex
+    complete = false;
 
+    $scope.isOpen = false; // whether to display myself as open or not    
+    $scope.toggleOpen = function() {
+        $scope.isOpen = !$scope.isOpen;
+    };
+    
+    $scope.showingInfo = false; // whether to display my info panel
+    $scope.toggleInfo = function() {
+        $scope.showingInfo = !$scope.showingInfo;
+    };
+
+    // if we need to change the top-level lex, use the emit structure
     $scope.retarget = function(newLexId) {
-        console.log('scope '+$scope.$id+' emitting retarget to '+newLexId);
+        console.log('scope '+$scope.$id+' emitting change to '+newLexId);
         $scope.$emit('retarget',newLexId);
     };
     
-    $scope.changeLex = function(id) {
-        console.log('scope '+$scope.$id+' changing lex to #'+id);
+    // fetch complete lex
+    $scope.fetchLex = function(id) {
+        console.log('scope '+$scope.$id+' fetching lex #'+id);
         $http.get("/reqage/api/lex/"+id)
             .success(function(response) {
                 $scope.myLex = response;
+            that.complete = true;
         });
     };
         
-    $scope.doRetarget = function(event, data) {
-        console.log('scope '+$scope.$id+' received message');
-        console.log('  event.targetScope='+event['targetScope'].$id);
-        console.log('  event.currentScope='+event['currentScope'].$id);
-        $scope.changeLex(data);
-    };
-    
+    $scope.setTop = function(id) {
+        console.log('scope '+$scope.$id+' is top level');
+        $scope.$on('retarget', function(event, data) { $scope.fetchLex(data); });
+        $scope.fetchLex(id);
+    }
+        
     init = function() {
-        console.log('new lexCtrl, scope='+$scope.$id);
-        if ($scope.lexId) {
-            $scope.changeLex($scope.lexId);
-            // I'm not the highest level so I shouldn't listen to re-targeting messages
-            console.log('scope '+$scope.$id +' not setting listener with lex '+$scope.lexId);
-        } else {
-            // we haven't been given a lexId, so we should use the global one
-            if (appLexId) {
-                $scope.changeLex(appLexId);
-                // I'm the highest level lex, so I listen for re-targeting
-                $scope.$on('retarget', function(event, data) { $scope.doRetarget(event, data); });
-                console.log('scope '+$scope.$id +' set listener with lex '+appLexId);
-            } else {
-                alert('no lex id set');
-            }
-        }
+//         console.log('scope '+$scope.$id+' has lexId '+$scope.lexId);
+//         if ($scope.lexId) {
+//             $scope.fetchLex($scope.lexId);
+//             // I'm not the highest level so I shouldn't listen to re-targeting messages
+//         } else {
+//             // we haven't been given a lexId, so we should use the global one
+//             if (appLexId) {
+//                 console.log('scope '+$scope.$id+' is highest level');
+//                 $scope.fetchLex(appLexId);
+//                 // I'm the highest level lex, so I listen for re-targeting
+//                 $scope.$on('retarget', function(event, data) { $scope.fetchLex(data); });
+//             } else {
+//                 alert('no lex id set');
+//             }
+//         }
     };
     
-    init(14);
+    init();
+    
 });
